b90ea48093a49400701c78d4a56d27681006042b
Martin Egger <38358157+megger-cgfx@users.noreply.github.com>
2022-05-16 15:06:33 +0200

    Removed GANdata

---
+++ houdini/temp/script/temp.py
@@ -0,0 +1,13 @@
+import time
+print("Started. ", time.time)
+time.sleep(10)
+print("Finished. ", time.time)
+"""
+print("Started. ", time.time)
+
+for i in range(10)
+    print("\twaited: ", i)
+    time.sleep(1)
+
+print("Finished. ", time.time)
+"""
